3f4ef7f9d241ea4da37d1a553b577249e6fa6619
narmanguebraun <narmanguebraun1@gmail.com>
2022-03-09 22:47:50 +0100

    access context - way 01

--- context-app/src/components/BookList.js
+++ context-app/src/components/BookList.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext'
 
 class BookList extends Component {
+  static contextType = ThemeContext;
   render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
     return (
-      <div className="book-list">
+      <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
         <ul>
-          <li>La promesse de l'aube</li>
-          <li>Les racines du ciel</li>
-          <li>La harpe de Birmanie</li>
+          <li style={{ background:theme.ui }}>La promesse de l'aube</li>
+          <li style={{ background:theme.ui }}>Les racines du ciel</li>
+          <li style={{ background:theme.ui }}>La harpe de Birmanie</li>
         </ul>
       </div>
     );
