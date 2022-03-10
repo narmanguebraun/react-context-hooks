8f1574db35c65fc63b97068260f998b7a6485b1f
narmanguebraun <narmanguebraun1@gmail.com>
2022-03-10 18:34:45 +0100

    add hooks-app start

---
+++ hooks-app/src/components/SongList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SongList = () => {
+  return (
+    <div className="song-list">
+      <ul>
+        <li>Motion - Peter Sandberg</li>
+        <li>Symphony No. 5 in C-Sharp Minor IV Adagietto - Gustav Mahler</li>
+      </ul>
+    </div>
+  );
+}
+
+export default SongList;
