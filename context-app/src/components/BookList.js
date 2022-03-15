2054460f79198b8b61dfa6cf784cd684c4841e2b
narmanguebraun <narmanguebraun1@gmail.com>
2022-03-15 09:18:35 +0100

    class component > functional component

--- context-app/src/components/BookList.js
+++ context-app/src/components/BookList.js
@@ -1,23 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext'
 
 // Context way 01
 
-class BookList extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-    return (
-      <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
-        <ul>
-          <li style={{ background:theme.ui }}>La promesse de l'aube</li>
-          <li style={{ background:theme.ui }}>Les racines du ciel</li>
-          <li style={{ background:theme.ui }}>La harpe de Birmanie</li>
-        </ul>
-      </div>
-    );
-  }
+// class BookList extends Component {
+//   static contextType = ThemeContext;
+//   render() {
+//     const { isLightTheme, light, dark } = this.context;
+//     const theme = isLightTheme ? light : dark;
+//     return (
+//       <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
+//         <ul>
+//           <li style={{ background:theme.ui }}>La promesse de l'aube</li>
+//           <li style={{ background:theme.ui }}>Les racines du ciel</li>
+//           <li style={{ background:theme.ui }}>La harpe de Birmanie</li>
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
+      <ul>
+        <li style={{ background:theme.ui }}>La promesse de l'aube</li>
+        <li style={{ background:theme.ui }}>Les racines du ciel</li>
+        <li style={{ background:theme.ui }}>La harpe de Birmanie</li>
+      </ul>
+    </div>
+  )
 }
 
 export default BookList;
